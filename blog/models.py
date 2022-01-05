097ca89d68946d8db21637f75145b60a0c6a30fc
E-Ozdemir <emreclarusway@gmail.com>
2022-01-05 21:26:32 +0100

    some changes

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,11 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=20)
     
-
+    class Meta:
+            verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     OPTIONS = (
@@ -15,7 +19,7 @@ class Post(models.Model):
     )
     title= models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to = '')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
@@ -25,3 +29,9 @@ class Post(models.Model):
     
     status = models.CharField(max_length=10, choices=OPTIONS,   default='draft')
     slug = models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
